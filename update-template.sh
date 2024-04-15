808c2c82c99d2bd82728cd036ebf8ff98b7cd1a1
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2024-04-14 23:58:21 -0400

    improve update script

--- update-template.sh
+++ update-template.sh
@@ -5,10 +5,11 @@
 if [ -d .git ]; then
   echo "ERROR: .git/ already exists, and I don't want to delete it if this script was called by accident."
   echo
-  echo "If you intend to pull one file, please rm -rf .git/ manually, then re-run."
+  echo "If you intend to download part of this repository, please rm -rf .git/ manually, then re-run."
   exit 1
 fi
 
+# https://stackoverflow.com/a/52269934
 git clone --depth 1  --filter=blob:none  --no-checkout \
   "https://github.com/konst-aa/fun.git"
 
@@ -16,14 +17,19 @@ mv fun/.git .
 rm -d fun
 
 # change stuff here, to taste
+TO_CHECKOUT=(
+    "README.md"
+    "LICENSE"
+    ".gitignore"
+)
 
-rm README.md LICENSE .gitignore
-git checkout main -- README.md LICENSE .gitignore
 
-# ok should be good now
+rm ${TO_CHECKOUT[@]}
+git checkout main -- ${TO_CHECKOUT[@]}
 
-mv REAMDE.md README-TEMP.md
+
+mv README.md README-TEMP.md
 echo "# THIS README IS FOR THE REPOSITORY THIS SCRIPT BELONGS TO: [https://github.com/konst-aa/fun]" > README.md
-echo "Please refer to this program's entry in the *rundown* section of the README" >> README.md
+echo "Please refer to this program's entry in the **rundown** section of the README" >> README.md
 cat README-TEMP.md >> README.md
 rm README-TEMP.md
