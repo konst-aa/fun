94b24c80506d33055592e96ecd0b656b541feaf2
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2024-04-30 15:05:30 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "konstantin astafurov's mini-projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
